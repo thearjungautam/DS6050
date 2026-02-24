6dda0bb42d49e862a8dd04f8ff6ff35851a8f933
Tom Lever <thomas.lever.business@gmail.com>
2026-02-24 01:09:47 -0500

    Create Assignment 3.

---
+++ Assignment_3/evaluate.py
@@ -0,0 +1,26 @@
+import torch
+
+
+def evaluate(model, iterator, criterion):
+    model.eval()
+    epoch_loss = 0
+
+    with torch.no_grad():
+        for i, batch in enumerate(iterator):
+            src, src_lengths, tgt = batch
+
+            # TODO: Assign to local variable `tensor_of_logits_for_many_tokens` the output of passing the source,
+            # the list of lengths, the target, and a teacher forcing ratio of 0 to the provided model.
+            raise NotImplementedError
+
+            number_of_tokens_in_vocabulary = tensor_of_logits_for_many_tokens.shape[-1]
+            tensor_of_logits_for_many_tokens = tensor_of_logits_for_many_tokens.reshape(-1, number_of_tokens_in_vocabulary)
+            tgt = tgt.reshape(-1)
+
+            # TODO: Assign to local variable loss the output of passing the tensor of logits for many tokens and the target
+            # to the provided criterion.
+            raise NotImplementedError
+
+            epoch_loss += loss.item()
+
+    return epoch_loss / len(iterator)
